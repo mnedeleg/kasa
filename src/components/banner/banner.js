60728b46bf6221234a0c43257c2aca13d50ceb09
M Nedeleg <nedellec.marion@gmail.com>
2022-10-29 11:46:31 +0200

    starting banner

---
+++ src/components/banner/banner.js
@@ -0,0 +1,17 @@
+import React, {useState, useEffect} from 'react';
+
+
+function Banner() {
+    return (
+        <div>
+            <div>
+                <img src="" alt="test"/>
+            </div>
+            <div>
+                <h1> Chez vous, partout et ailleurs </h1>
+            </div>
+        </div>
+    )
+}
+
+export default Banner;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import NavBar from "../components/navBar/navBar"
+import Banner from "../components/banner/banner"
 import AppartmentList from "../components/Appartment/AppartmentList"
 const Home = () => {
     return (
         <div>
-        
-            <div>
-                <h1> Chez vous, partout et ailleurs </h1>
-            </div>
+            <Banner/>
             <AppartmentList/>
         </div>
     );
