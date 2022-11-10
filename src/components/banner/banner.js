7654f977be391342142e97dbead83ac1c9b17ceb
M Nedeleg <nedellec.marion@gmail.com>
2022-11-10 15:42:55 +0100

    banner + flat home page image

--- src/components/banner/banner.js
+++ src/components/banner/banner.js
@@ -4,11 +4,7 @@ import BannerImg from "../../image/banner-img.jpg";
 function Banner() {
     return (
         <div className= 'banner'>
-            {/* <div>
-                <img className='banner-img' src= {BannerImg} alt="image de bord de mer dans le brouillard"/>
-            </div> */}
             <h1 className='banner-title'> Chez vous, partout et ailleurs </h1>
-            {/* <div className="overlay"></div> */}
         </div>
     )
 }
