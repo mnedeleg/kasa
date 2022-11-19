6122bc772465b27686ce56bb308851f2627c9ca0
M Nedeleg <nedellec.marion@gmail.com>
2022-11-19 13:03:48 +0100

    about page started with collapses

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,13 +1,31 @@
 import About from "../components/About/About"
+import AboutInfo from "../data/about-data.json"
 import CollapseDescription from "../components/Appartment/CollapseDescription"
 
 const AboutPage = () => {
 
+   
     return (
         <div>
-             {/* <CollapseDescription title="Description">
-                 {appt.description}
-            </CollapseDescription> */}
+             <CollapseDescription title="Fiabilité">
+     
+        
+            </CollapseDescription>
+
+            <CollapseDescription title="Respect">
+     
+        
+            </CollapseDescription>
+
+            <CollapseDescription title="Service">
+     
+        
+            </CollapseDescription>
+
+            <CollapseDescription title="Sécurité">
+     
+        
+            </CollapseDescription>
         </div>
     );
 }
