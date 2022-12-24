19108809172f97ca718e13a643b4d49b09d7fe38
M Nedeleg <nedellec.marion@gmail.com>
2022-12-24 16:16:09 +0100

    arrow one pic - icon review carousel

--- src/components/Appartment/Carousel.js
+++ src/components/Appartment/Carousel.js
@@ -5,8 +5,7 @@ import { FaChevronLeft } from "react-icons/fa";
 const Carousel = ({ slides }) => {
     const [current, setCurrent] = useState(0);
     const picturesTab = slides;
-    const length = picturesTab.length;
-  
+    const length = picturesTab.length; 
 
     const nextPicture = () => {
         setCurrent(current === length -1 ? 0 : current + 1)
@@ -16,17 +15,50 @@ const Carousel = ({ slides }) => {
         setCurrent(current === 0 ? length -1 : current - 1)
     }
 
+    // if(length === 1) return <OnePicture src={slides[0]} />
+    console.log(slides)
     return (
+        
         <div className='carousel'>
             <div className='img-container'> 
                 <img className= "img-containter-single" src= {slides[current]} alt="images de l'intérieur de l'appartement"/>
                 <div className="indicator" >{current + 1}/{length}</div>
             </div>    
-            <div>
-                <FaChevronLeft className='left-arrow' onClick={prevPicture}/>
-                <FaChevronRight className='right-arrow' onClick={nextPicture}/>
-            </div>
+            <NavigationArrow slides = {slides} prevPicture={prevPicture} nextPicture={nextPicture}/>
+            {/* {
+                slides.length > 1 && (
+                    <div>
+                    <FaChevronLeft className='left-arrow' onClick={prevPicture}/>
+                    <FaChevronRight className='right-arrow' onClick={nextPicture}/>
+                </div>
+                )
+            } */}
+
         </div>
     )};
+
+    const NavigationArrow = ({slides, nextPicture, prevPicture}) => {
+        console.log(slides)
+        if (slides.length > 1)  {
+            return (
+                <div>
+                    <FaChevronLeft className='left-arrow' onClick={prevPicture}/>
+                    <FaChevronRight className='right-arrow' onClick={nextPicture}/>
+                </div>
+            )
+        }else{
+            return null
+        }
+        }
+    
+
+    // const OnePicture = ({src}) => (
+    //     <div className='carousel'>
+    //         <div className='img-container'> 
+    //             <img className= "img-containter-single" src= {src} alt="images de l'intérieur de l'appartement"/>
+
+    //         </div>    
+    //     </div>
+    // )
 
 export default Carousel
